e7bc0f4a7e322d23ab3ec5f621425baeceb2b5d7
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-05-23 10:42:04 +0600

    Add initial implementation of LangChain chat models and related assets

--- chapter-2-LangChain Chat Models/2.2.understanding_message_types.py
+++ chapter-2-LangChain Chat Models/2.2.understanding_message_types.py
@@ -1,13 +1,6 @@
 import getpass
 import os
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
-
-
-# model_anthropic = ChatAnthropic(model="claude-2", temperature=0.9, api_key="sk-ant-")
-# model_openai = ChatOpenAI(model="gpt-4o", temperature=0.9, api_key="sk-proj-")
-
 
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
@@ -20,11 +13,3 @@ model_google = ChatGoogleGenerativeAI(
     max_retries=2,
     api_key=os.environ["GOOGLE_API_KEY"],
 )
-
-
-# 1 prompt 1 response
-"""
-# response = model_google.invoke("What is the capital of Bangladesh?") # 1 prompt 1 response
-# print(response.content)
-"""
-
---
+++ chapter-2-LangChain Chat Models/readme.md
@@ -0,0 +1,4 @@
+
+![alt text](2.1.1.How_LLM_Chat_Model_Takes_Input_and_Generates_Output.png)
+
+![alt text](2.1.2.LLM_Chat_Processing_Architecture.png)
