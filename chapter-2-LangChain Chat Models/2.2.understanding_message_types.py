17912e0d6dc549822cda9b6965bda3a89c11e0ac
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-05-23 11:48:33 +0600

    Add functionality to invoke Google AI model with Bangla language prompts

--- chapter-2-LangChain Chat Models/2.2.understanding_message_types.py
+++ chapter-2-LangChain Chat Models/2.2.understanding_message_types.py
@@ -1,6 +1,7 @@
 import getpass
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
@@ -13,3 +14,13 @@ model_google = ChatGoogleGenerativeAI(
     max_retries=2,
     api_key=os.environ["GOOGLE_API_KEY"],
 )
+
+systemInstruction = SystemMessage(content="Reply every prompt in bangla Language.")
+userMessage = HumanMessage(content="what is the independence day of Bangladesh?")
+
+response = model_google.invoke([
+    systemInstruction,
+    userMessage
+])
+
+print(response.content[0]['text'])
