e1339175518fefaf7455030d6a42b0bd2f322a3b
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-06-03 16:49:26 +0600

    Implement environment loading and enhance FewShotChatMessagePromptTemplate with OpenAI and Google models

--- chapter-3-LangChain Prompt Templates/3.7.0.fewshot_chat_template.py
+++ chapter-3-LangChain Prompt Templates/3.7.0.fewshot_chat_template.py
@@ -4,11 +4,32 @@ Chat Fewshot Template [Steps]
     - We will the create an instance of our fewshot template, but this time using the FewShotChatMessagePromptTemplate and without the suffix, as the suffix is already defined in the example formatter.
     - We will then create a chat prompt template, and place the fewshot prompt before our user message to the model.
 """
-from langchain_core.prompts import FewShotChatMessagePromptTemplate, ChatPromptTemplate
+import os
+from load_env import load_env
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_core.prompts import FewShotChatMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate, SystemMessagePromptTemplate
+
+load_env()
+
+model = ChatOpenAI(
+    openai_api_base="https://openrouter.ai/api/v1",
+    model_name="openai/gpt-oss-120b:free",
+    temperature=0.7,
+    max_tokens=100,
+    openai_api_key=os.environ["OPEN_ROUTER_API_KEY"],
+)
+
+model_google = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    api_key=os.environ["GOOGLE_API_KEY"],
+    temperature=0,
+)
 
 example_formatter = ChatPromptTemplate.from_messages([
-    ("human", "{input}"),
-    ("ai", "{output}")
+    HumanMessagePromptTemplate.from_template("{input}"),
+    AIMessagePromptTemplate.from_template("{output}")
 ])
 
 example_set = [
@@ -32,9 +53,9 @@ few_shot_template = FewShotChatMessagePromptTemplate(
 )
 
 main_prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant that can answer questions about basic math."),
+    SystemMessagePromptTemplate.from_template("You are a helpful assistant that can answer questions about basic math."),
     few_shot_template,
-    ("human", "{user_query}")
+    HumanMessagePromptTemplate.from_template("{user_query}")
 ])
 
 invoked_prompt = main_prompt.invoke({
@@ -42,4 +63,24 @@ invoked_prompt = main_prompt.invoke({
 })
 
 # print(invoked_prompt)
-print(invoked_prompt.to_string())
+# print(invoked_prompt.to_string())
+
+
+"""
+response = model_google.invoke(invoked_prompt)
+print(response.content[0].text)
+"""
+
+"""
+formatted_prompt = main_prompt.format(user_query="6 ribbit 5")
+response = model_google.invoke(formatted_prompt)
+print(response.content)
+"""
+
+# LCEL = LangChain Expression Language
+
+chain = main_prompt | model_google
+response = chain.invoke({
+    "user_query": "2 ribbit 7"
+})
+print(response.content)
