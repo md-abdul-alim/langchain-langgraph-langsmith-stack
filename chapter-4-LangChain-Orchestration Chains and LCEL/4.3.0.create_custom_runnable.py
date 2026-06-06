e48d5d454c279940d86a60e004f50809e499db0c
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-06-06 15:53:11 +0600

    regular functions to runnables

--- chapter-4-LangChain-Orchestration Chains and LCEL/4.3.0.create_custom_runnable.py
+++ chapter-4-LangChain-Orchestration Chains and LCEL/4.3.0.create_custom_runnable.py
@@ -21,3 +21,13 @@ print(invoke_result)  # Output: 10
 
 batch_result = runnable_multiply.batch([1, 2, 3])
 print(batch_result)  # Output: [2, 4, 6]
+
+# Demo 2: Regular functions to runnables
+def reverse_text_function(text: str) -> str:
+    return text[::-1]
+
+reverse_text_runnable = RunnableLambda(reverse_text_function)
+result = reverse_text_runnable.invoke("Hello, World!")
+print(result)  # Output: !dlroW ,olleH
+
+
