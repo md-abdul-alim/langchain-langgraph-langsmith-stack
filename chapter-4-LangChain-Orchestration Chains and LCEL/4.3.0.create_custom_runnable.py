45728102ec0f1ab9e913f6c78a6736af68a08aea
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-06-06 16:00:33 +0600

    Async functions to runnables

--- chapter-4-LangChain-Orchestration Chains and LCEL/4.3.0.create_custom_runnable.py
+++ chapter-4-LangChain-Orchestration Chains and LCEL/4.3.0.create_custom_runnable.py
@@ -1,17 +1,6 @@
-import os
-from load_env import load_env
-from langchain_openai import ChatOpenAI
+import asyncio
 from langchain_core.runnables import RunnableLambda
 
-load_env()
-
-model = ChatOpenAI(
-    openai_api_base="https://openrouter.ai/api/v1",
-    model_name="openai/gpt-oss-120b:free",
-    temperature=0.7,
-    max_tokens=100,
-    openai_api_key=os.environ["OPEN_ROUTER_API_KEY"],
-)
 
 # Demo 1: Lambda functions to runnables
 runnable_multiply = RunnableLambda(lambda x: x * 2)
@@ -20,7 +9,7 @@ invoke_result = runnable_multiply.invoke(5)
 print(invoke_result)  # Output: 10
 
 batch_result = runnable_multiply.batch([1, 2, 3])
-print(batch_result)  # Output: [2, 4, 6]
+print(batch_result, "\n")  # Output: [2, 4, 6]
 
 # Demo 2: Regular functions to runnables
 def reverse_text_function(text: str) -> str:
@@ -28,8 +17,15 @@ def reverse_text_function(text: str) -> str:
 
 reverse_text_runnable = RunnableLambda(reverse_text_function)
 result = reverse_text_runnable.invoke("Hello, World!")
-print(result)  # Output: !dlroW ,olleH
+print(result, "\n")  # Output: !dlroW ,olleH
 
 
 text_batch_result = reverse_text_runnable.batch(["Hello", "World"])
-print(text_batch_result)  # Output: ['olleH', 'dlroW']
+print(text_batch_result, "\n")  # Output: ['olleH', 'dlroW']
+
+# Demo 3: Async functions to runnables
+async def async_reverse_text_function(text: str) -> str:
+    aresult = await reverse_text_runnable.ainvoke(text)
+    print(f"Async result: {aresult}")  # Output: Async result: !dlroW ,olleH
+
+asyncio.run(async_reverse_text_function("Hello, World!"))
