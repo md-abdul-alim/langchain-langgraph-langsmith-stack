4e8736f96b214b198e3e28fb855b4d90714c5dea
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-06-10 22:47:03 +0600

    Update model name and enhance output formatting in runnable demos

--- chapter-4-LangChain-Orchestration Chains and LCEL/4.6.0.combining_runnables.py
+++ chapter-4-LangChain-Orchestration Chains and LCEL/4.6.0.combining_runnables.py
@@ -8,7 +8,7 @@ load_env()
 
 model = ChatOpenAI(
     openai_api_base="https://openrouter.ai/api/v1",
-    model_name="openai/gpt-oss-120b:free",
+    model_name="openrouter/owl-alpha",
     temperature=0.7,
     max_tokens=100,
     openai_api_key=os.environ["OPEN_ROUTER_API_KEY"],
@@ -22,6 +22,7 @@ simple_chain = prompt | model | output_parser
 
 result = simple_chain.invoke({"topic": "LangChain"})
 print(result, "\n")
+print("="*50 + '\n')
 
 # Demo 2 - Chain to Runnable
 
@@ -30,3 +31,20 @@ combined_chain = simple_chain | (lambda chain_output: chain_output + " Oh wow, t
 combined_chain_result = combined_chain.invoke({"topic": "LangChain"})
 print('--- Combined Chain Result ---')
 print(combined_chain_result, "\n")
+print("="*50 + '\n')
+
+# Demo 3 - Chain to Chain
+
+fact_checking_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that fact-checks statements."),
+    ("user", "Is it true that {statement}?")
+])
+
+checker_chain = fact_checking_prompt | model | output_parser
+
+fact_checking_chain = { "statement": simple_chain } | checker_chain
+
+dual_chain_result = fact_checking_chain.invoke({"topic": "LangChain is a programming language."})
+print('--- Dual Chain Result ---')
+print(dual_chain_result, "\n")
+print("="*50 + '\n')
